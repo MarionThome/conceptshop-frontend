8a235d3831e713599acc15507e2f6f74d398b26c
Marion <thm.marion@gmail.com>
2023-02-02 17:51:36 +0100

    out of order css style

--- components/ProductSnapShot.js
+++ components/ProductSnapShot.js
@@ -1,25 +1,29 @@
 import Button from "./Button";
-import styles from "../styles/ProductSnapShot.module.css"
+import styles from "../styles/ProductSnapShot.module.css";
 import { useEffect, useState } from "react";
 
-export default function ProductSnapShot(props){
-    const [image, setImage] = useState("")
-    useEffect(() =>{
-        const fileImg = require(`../assets/${props.data.name}.png`)
-        console.log(fileImg.default.src)
+export default function ProductSnapShot(props) {
+  const [image, setImage] = useState("");
+  useEffect(() => {
+    const fileImg = require(`../assets/${props.data.name}.png`);
+    console.log(fileImg.default.src);
 
-        setImage(fileImg.default.src)
-    }, [])
-    return (
-        <div className={styles.infosContainer}>
-            <img src={image}/>
-            <div className={styles.itemInfos}>
-            <h3>{props.data.name}</h3>
-            <p>{props.data.description}</p>
-            <div>
-            <Button name={"Buy now"} padding = {"10px 20px"}/>
-            </div>
-            </div>
+    setImage(fileImg.default.src);
+  }, []);
+  return (
+    <div className={styles.infosContainer}>
+      <img src={image} />
+      <div className={styles.itemInfos}>
+        <h3>{props.data.name}</h3>
+        <p>{props.data.description}</p>
+        <p style={{color : !props.data.availability && "red" }}>
+          <span style={{ textDecoration: !props.data.availability ? "line-through" : "none"}}>{props.data.price}</span> €
+          {!props.data.availability && " out of order"}
+        </p>
+        <div>
+          <Button name={"Buy now"} padding={"10px 20px"} />
         </div>
-    )
+      </div>
+    </div>
+  );
 }
